5aebcaf16a7b4c485e68e643ad7436023d7adc30
InjoyDeng <dengyinzhong@tinman.cn>
2024-08-01 19:22:30 +0800

    fix: migration error by updating role column in apps_users table

--- db/migrate/20240516050530_add_role_to_users_apps.rb
+++ db/migrate/20240516050530_add_role_to_users_apps.rb
@@ -2,8 +2,11 @@ class AddRoleToUsersApps < ActiveRecord::Migration[7.1]
   def up
     add_column :apps_users, :role, :integer, null: false, default: 0
 
-    # all preview apps were created by owner.
-    Collaborator.update_all(role: Collaborator.roles[:admin])
+    # 使用 SQL 语句更新 apps_users 表中的数据
+    execute <<-SQL
+      UPDATE apps_users
+      SET role = #{Collaborator.roles[:admin]}
+    SQL
   end
 
   def down
